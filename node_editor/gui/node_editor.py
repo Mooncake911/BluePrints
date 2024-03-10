4a2c247c3c6fad280a6fbf5e9f90c24b030f4676
Vadim Shaidurov <shaidurovvadik@yandex.ru>
2024-03-11 00:06:38 +0300

    (Preview release)

--- node_editor/gui/node_editor.py
+++ node_editor/gui/node_editor.py
@@ -62,14 +62,14 @@ class NodeEditor(QtCore.QObject):
             if event.key() == QtCore.Qt.Key.Key_Delete:
                 for item in self.scene.selectedItems():
                     item.delete()
+                return True
 
             if event.key() == QtCore.Qt.Key.Key_A:
                 node_items = [item for item in self.scene.items() if isinstance(item, Node)]
                 all_selected = len(self.scene.selectedItems()) == len(node_items)
                 for item in node_items:
                     item.setSelected(not all_selected)
-
-            return True
+                return True
 
         elif event.type() == QtCore.QEvent.Type.GraphicsSceneMouseMove:
             if self.connection:
